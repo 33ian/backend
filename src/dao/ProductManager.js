81d267a0a73e8aa14ad3d9752d01f57d5ac66faf
Ian Vezzoso <ianvezzoso@gmail.com>
2023-07-11 18:01:34 -0300

    products pre-entrega1

--- src/dao/ProductManager.js
+++ src/dao/ProductManager.js
@@ -1,148 +1,109 @@
 import fs from "fs";
 import { __dirname } from "../utils.js";
 import path from "path";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 export class ProductManager {
     constructor(fileName) {
         this.path = path.join(__dirname, `./files/${fileName}`)
-        // this.products = [];
-        // this.loadProducts();
+        this.products = []
+        this.loadProducts();
     };
 
-    fileExists(){
+    fileExists() {
         return fs.existsSync(this.path);
     }
 
-    async get(){
-        try{
-            if(this.fileExists()){
-                const content = await fs.promises.readFile(this.path, "utf-8");
-                const products = JSON.parse(content);
-                return products;
-            }else{
-                throw new Error("No es posbile obtener los productos",)
+    async loadProducts() {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf8');
+            if (data) {
+                this.products = JSON.parse(data);
             }
-        }catch(error){
-            throw error;
+            console.log("loadProducts: ", this.products);
+            return this.products;
+        } catch (error) {
+            throw new Error("Hubo un error al cargar los productos", error);
         }
     }
 
-    async getById(id){
-        //devuelve el producto que cumple con el id recibido
+generateId() {
+    let newId = uuidv4();
+    return newId
+}
+
+async save() {
+    try {
+        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+    } catch (error) {
+        console.error('Hubo un error al guardar los productos:', error);
+    }
+}
+
+addProduct(product) {
+    if(!product.title || product.title == "" || !product.description || product.description == "" || !product.price || product.price <= 0 || !product.thumbnail || product.thumbnail == "" || !product.code || product.code == "" || !product.stock || !product.status || product.status == false || !product.category || product.category == "") {
+        console.error('Todos los campos son obligatorios');
+        return;
+    }
+
+    const existingProduct = this.products.find((p) => p.code === product.code);
+    if (existingProduct) {
+        console.error('Ya existe este producto');
+        return;
+    }
+
+    const newProduct = {
+        id: this.generateId(),
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        thumbnail: product.thumbnail,
+        code: product.code,
+        stock: product.stock,
+        status: product.status,
+        category: product.category
     };
 
-    async save(product){
-        try{
-            if(this.fileExists()){
-                const content = await fs.promises.readFile(this.path, "utf-8");
-                const products = JSON.parse(content);
-                // let newId =  1;
-                //     if(products.length >0){
-                //         newId = products[products.length-1].id+1;
-                //     }
-                let newId = uuidv4();
-                const newProduct = {
-                    id:newId,
-                    ...product
-                };
-                products.push(newProduct);
-                await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
-                return newProduct;
-            }else{
-                throw new Error("No es posbile esta operación",)
-            }
-        }catch(error){
-            throw error;
+    this.products.push(newProduct);
+    this.save();
+    return newProduct;
+}
+
+getProducts() {
+    return this.products;
+}
+
+    async getById(id) {
+        const product = this.products.find((p) => p.id === id);
+        if (!product) {
+            console.error('No se ha encontrado el producto');
+            return;
         }
+        return product;
+    };
+
+    updateProduct(id, updatedFields) {
+        const productIndex = this.products.findIndex((p) => p.id === id);
+        if (productIndex === -1) {
+            console.error('No se ha encontrado el producto');
+            return;
+        } else {
+            console.log("Producto a actualizar encontrado correctamente");
+        }
+
+        const updatedProduct = { ...this.products[productIndex], ...updatedFields };
+        this.products[productIndex] = updatedProduct;
+        this.save();
     }
 
-    // async loadProducts() {
-    //     try {
-    //         const data = await fs.promises.readFile(this.path, 'utf8');
-    //         if (data) {
-    //             this.products = JSON.parse(data);
-    //         }
-    //         console.log("loadProducts: ", this.products);
-    //         return this.products;
-    //     } catch (err) {
-    //         throw new Error("Hubo un error al cargar el producto", err);
-    //     }
-    // }
+    deleteProduct(id) {
+        const productIndex = this.products.findIndex((p) => p.id === id);
+        if (productIndex === -1) {
+            console.error('No se ha encontrado el producto');
+            return;
+        }
 
-//     async saveProducts() {
-//         try {
-//             await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-//         } catch (err) {
-//             throw new Error("Hubo un error al guardar el producto", err);
-//         }
-//     }
-
-//     generateId() {
-//         return this.products.length > 0
-//             ? Math.max(...this.products.map((product) => product.id)) + 1
-//             : 1;
-//     }
-
-//     addProduct(product) {
-//         if (!product.title || product.title == "" || !product.description || product.description == "" || !product.price || product.price == 0 || !product.thumbnail || product.thumbnail == "" || !product.code || product.code == "" || !product.stock) {
-//             throw new Error("Todos los campos son requeridos");
-            
-//         }
-
-//         const existingProduct = this.products.find((p) => p.code === product.code);
-//         if (existingProduct) {
-//             throw new Error("Ya existe un producto con el mismo código");
-//         }
-
-//         const newProduct = {
-//             id: this.generateId(),
-//             title: product.title,
-//             description: product.description,
-//             price: product.price,
-//             thumbnail: product.thumbnail,
-//             code: product.code,
-//             stock: product.stock,
-//         };
-
-//         this.products.push(newProduct);
-//         this.saveProducts();
-//     }
-
-//     getProducts() {
-//         return this.products;
-//     }
-
-//     getProductById(id) {
-//         const product = this.products.find((p) => p.id === id);
-//         if (!product) {
-//             throw new Error("No se pudo encontrar el producto");
-//         }
-//         return product;
-//     }
-
-//     updateProduct(id, updatedFields) {
-//         const productIndex = this.products.findIndex((p) => p.id === id);
-//         if (productIndex === -1) {
-//             throw new Error("No se pudo encontrar el producto");
-            
-//         } else {
-//             console.log("Producto para update encontrado!");
-//         }
-
-//         const updatedProduct = { ...this.products[productIndex], ...updatedFields };
-//         this.products[productIndex] = updatedProduct;
-//         this.saveProducts();
-//     }
-
-//     deleteProduct(id) {
-//         const productIndex = this.products.findIndex((p) => p.id === id);
-//         if (productIndex === -1) {
-//             throw new Error("No se pudo encontrar el producto");
-            
-//         }
-
-//         this.products.splice(productIndex, 1);
-//         this.saveProducts();
-//     }
+        this.products.splice(productIndex, 1);
+        this.save();
     }
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,25 +1,35 @@
 import { Router } from "express";  
 import { ProductManager } from "../dao/ProductManager.js";
+import { __dirname } from '../utils.js';
 
 const productService = new ProductManager('products.json')
+const router = Router();
 
 const validateFields = (req, res, next)=>{
     const productInfo = req.body;
-    if(!productInfo.title || productInfo.title == "" || !productInfo.description || productInfo.description == "" || !productInfo.price || productInfo.price == 0 || !productInfo.thumbnail || productInfo.thumbnail == "" || !productInfo.code || productInfo.code == "" || !productInfo.stock || !productInfo.status || productInfo.status == false || !productInfo.category || productInfo.category == ""){
+    if(!productInfo.title || productInfo.title == "" || !productInfo.description || productInfo.description == "" || !productInfo.price || productInfo.price <= 0 || !productInfo.thumbnail || productInfo.thumbnail == "" || !productInfo.code || productInfo.code == "" || !productInfo.stock || !productInfo.status || productInfo.status == false || !productInfo.category || productInfo.category == ""){
         return res.json({status: "error", message:"Todos los campos son obligatorios"})
     }else{
         next();
     }   
 }
 
-const router = Router();
+
 
 router.get("/",async(req,res)=>{
     try{
         const limit = req.query.limit;
-        const products = await productService.get();
+        const products = await productService.loadProducts();
+        let resultado = 0;
         if(limit){
-
+            const limite = parseInt(req.query.limit);
+            console.log("limite: ", limite);
+            if (limite > 0) {
+                resultado = products.slice(0,limite);
+            } else {
+                resultado = products;
+            }
+            res.send(resultado);
         }else{
             res.json({status:"success", data:products})
         }
@@ -27,21 +37,44 @@ router.get("/",async(req,res)=>{
         res.json({status:"error", message: error.message});
     }
 });
-router.get("/:pid",(req,res)=>{});
-router.post("/", validateFields, async(req, res)=>{
-    try{
-        const productInfo = req.body;
-        const productCreated = await productService.save(productInfo);
-        res.json({statis:"success", data:productCreated, message:"Producto creado"})
-    }catch(error){
-        res.json({status:"error", message: error.message});
+router.get("/:pid",async(req,res)=>{
+    try {
+        let pid = req.params.pid;
+        let result = await productService.getById(pid);
+        res.json({ status: 'success', data: result });
+    } catch (error) {
+            res.json({ status: 'error', message: error.message });
+        
     }
 });
-router.put("/:pid", validateFields ,(req,res)=>{
-    const productInfo = req.body;
+router.post("/", validateFields, async (req, res) => {
+    try {
+        const productInfo = req.body;
+        const productCreated =  await productService.addProduct(productInfo);
+        res.json({ status: "success", data: productCreated, message: "Producto creado con exito" });
+    } catch (error) {
+        res.json({ status: "error", message: error.message });
+    }
+});
+router.put("/:pid", validateFields,  (req, res) => {
+    try {
+        let pid = req.params.pid;
+        let product = req.body;
+        let result =  productService.updateProduct(pid, product);
+        result.id = pid;
+        res.json({ status: 'success', data: result });
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+    }
 });
 router.delete("/:pid",(req,res)=>{
-
+    try {
+        let pid = req.params.pid;
+        let result =  productService.deleteProduct(pid);
+        res.json({ status: "success", data: deleteProduct, message: "Producto eliminado con exito" });
+    } catch (error) {
+        res.json({ status: 'error', message: error.message });
+    }
 })
 
 export {router as productsRouter}
